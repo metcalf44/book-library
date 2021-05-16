203465e1f45aeb115978e63d141579de6e8c785e
Ernie <emetcalf44@gmail.com>
2021-05-16 15:47:06 +0100

    setup complete

---
+++ scripts/drop-database.js
@@ -0,0 +1,38 @@
+
+const mysql = require('mysql2/promise');
+const path = require('path');
+const envFile = '../.env.test';
+
+require('dotenv').config({
+    path: path.join(__dirname, envFile),
+});
+
+const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+
+const dropDb = async () => {
+    try {
+        const db = await mysql.createConnection({
+            host: DB_HOST,
+            user: DB_USER,
+            password: DB_PASSWORD,
+            port: DB_PORT,
+        });
+
+        await db.query(`DROP DATABASE ${DB_NAME}`);
+        db.close();
+    } catch (err) {
+        console.log(
+            'Your environment variables may be wrong. Please double check your .env file'
+        );
+
+        console.log('Environment variables are:', {
+            DB_PASSWORD,
+            DB_NAME,
+            DB_USER,
+            DB_PORT,
+        })
+        console.log(err)
+    }
+};
+
+dropDb();
